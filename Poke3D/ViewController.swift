5316f98e94c3447bc4088b3cc1e4287027a62169
Hrutujaa21 <kamthe.hrutuja@gmail.com>
2025-01-03 19:46:46 +0530

    Adding 3D pokemon modes to card tracking

--- Poke3D/ViewController.swift
+++ Poke3D/ViewController.swift
@@ -72,6 +72,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
             node.addChildNode(planeNode)
             
+            if imageAnchor.referenceImage.name == "eevee-card" {
+                
+                if let pokeScene = SCNScene(named: "art.scnassets/eevee.scn") {
+                    
+                    if let pokeNode = pokeScene.rootNode.childNodes.first {
+                        
+                        planeNode.eulerAngles.x = .pi/2
+                        
+                        planeNode.addChildNode(pokeNode)
+                        
+                    }
+                    
+                }
+            }
+            
         }
         
         return node
