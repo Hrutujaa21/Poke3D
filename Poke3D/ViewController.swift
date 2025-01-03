ce08899a9693ed8305f69e10b7786e402d71d9ae
Hrutujaa21 <kamthe.hrutuja@gmail.com>
2025-01-03 19:50:52 +0530

    Tracking multiple pokemons

--- Poke3D/ViewController.swift
+++ Poke3D/ViewController.swift
@@ -30,13 +30,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARImageTrackingConfiguration()
+        let configuration = ARWorldTrackingConfiguration()
         
         if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main) {
             
-            configuration.trackingImages = imageToTrack
+            configuration.detectionImages = imageToTrack
             
-            configuration.maximumNumberOfTrackedImages = 1
+            configuration.maximumNumberOfTrackedImages = 2
             
             print("Images Added Successfully")
             
@@ -75,6 +75,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             if imageAnchor.referenceImage.name == "eevee-card" {
                 
                 if let pokeScene = SCNScene(named: "art.scnassets/eevee.scn") {
+                    
+                    if let pokeNode = pokeScene.rootNode.childNodes.first {
+                        
+                        planeNode.eulerAngles.x = .pi/2
+                        
+                        planeNode.addChildNode(pokeNode)
+                        
+                    }
+                    
+                }
+            }
+            
+            if imageAnchor.referenceImage.name == "oddish-card" {
+                
+                if let pokeScene = SCNScene(named: "art.scnassets/oddish.scn") {
                     
                     if let pokeNode = pokeScene.rootNode.childNodes.first {
                         
